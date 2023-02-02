8d1ed327d47ab5a6b8cf8d7a4a58d8a9d2eb40ef
Gustavo <gustavo.om1206@gmail.com>
2023-02-01 22:29:30 -0300

    commit

--- exercicios-matematica/src/Main.java
+++ exercicios-matematica/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, d, diferenca;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        diferenca = (a * b - c * d);
+        System.out.println("Diferença = " + diferenca);
+
+        sc.close();
     }
 }
