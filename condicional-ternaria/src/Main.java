aea632896743b5608dfbcf5fe2dd858cf184018c
Gustavo Martins <gustavo.om1206@gmail.com>
2023-02-02 16:18:51 -0300

    commit

---
+++ condicional-ternaria/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean condicao = (3 > 2);
+       String nome = condicao ? "Gustavo" : "Sophia";
+       System.out.println(nome);
+       int idade = condicao ? 16 : 17;
+       System.out.println(idade);
+    }
+}
