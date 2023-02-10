f19f5c4dc5d0d9c6775499ae138e8ac217ddf34c
Gustavo <gustavo.om1206@gmail.com>
2023-02-10 15:51:00 -0300

    commit

--- data-hora/src/application/Program.java
+++ data-hora/src/application/Program.java
@@ -45,13 +45,21 @@ public class Program {
         LocalDate d01 = LocalDate.parse("2023-02-08");
         LocalDateTime d02 = LocalDateTime.parse("2023-02-08T20:30:00");
         Instant d03 = Instant.parse("2023-02-08T20:30:00Z");
+        LocalDate r1 = LocalDate.ofInstant(d03,ZoneId.systemDefault());
+        LocalDateTime r2 = LocalDateTime.ofInstant(d03,ZoneId.systemDefault());
 
         System.out.println("Data 1 -> " + d01.format(fmt1));
-        System.out.println("Data 1 -> " + fmt1.format(d01));
-        System.out.println("Data 1 -> " + d01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         System.out.println("-----------------------");
         System.out.println("Data 2 -> " + d02.format(fmt2));
         System.out.println("----------------------------");
         System.out.println("Data 3 -> " + fmt3.format(d03));
+        System.out.println("----------------------------");
+        System.out.println("Data 4 -> " + r1);
+        System.out.println("----------------------------");
+        System.out.println("Data 5 -> " + r2);
+        System.out.println("----------------------------");
+        System.out.println("Dia -> " + r2.getDayOfMonth());
+        System.out.println("Mês -> " + r2.getMonthValue());
+        System.out.println("Ano -> " + r2.getYear());
     }
 }
