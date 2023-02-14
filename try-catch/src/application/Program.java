f7fb6f723ccd695405af2631671cbf2050923aaf
Gustavo <gustavo.om1206@gmail.com>
2023-02-14 18:37:38 -0300

    commit

--- try-catch/src/application/Program.java
+++ try-catch/src/application/Program.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+    }
+
+    public static void method1(){
+        System.out.println("***Method1 Start***");
+        method2();
+        System.out.println("***Method1 End***");
+    }
+
+    public static void method2(){
+        System.out.println("***Method2 Start***");
         Scanner sc = new Scanner(System.in);
 
         try {
@@ -13,11 +25,13 @@ public class Program {
             System.out.println(vect[position]);
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Invalid position!");
+            e.printStackTrace();
+            sc.next();
         }catch (InputMismatchException e){
             System.out.println("Invalid number!");
         }
 
-        System.out.println("End of program");
         sc.close();
+        System.out.println("***Method2 End***");
     }
 }
