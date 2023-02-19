ad2f632d075fd676833ed9ed5d05a9d58c2cb3f1
Gustavo <gustavo.om1206@gmail.com>
2023-02-18 21:51:44 -0300

    commit

--- composicao-exercicio-2/src/application/Program.java
+++ composicao-exercicio-2/src/application/Program.java
@@ -17,51 +17,34 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
         System.out.println("Enter client data:");
-
         System.out.print("Name: ");
         String clientName = sc.nextLine();
-
         System.out.print("Email: ");
         String clientEmail = sc.nextLine();
-
         System.out.print("Birth date (DD/MM/YYYY): ");
         Date birthDate = sdf.parse(sc.next());
-
         Client client = new Client(clientName, clientEmail, birthDate);
-
         System.out.println("Enter order data:");
-
         System.out.print("Status: ");
         OrderStatus status = OrderStatus.valueOf(sc.next());
-
         Order order = new Order(new Date(), status, client);
-
         System.out.print("How many items to this order? ");
         int items = sc.nextInt();
-
         for (int i=0;i<items;i++){
             System.out.printf("Enter #%d item data:%n", i+1);
-
             System.out.print("Product name: ");
             String name = sc.next();
-
             System.out.print("Product price: ");
             double price = sc.nextDouble();
-
             System.out.print("Quantity: ");
             int quantity = sc.nextInt();
-
             Product product = new Product(name, price);
-
             OrderItem item = new OrderItem(quantity, price, product);
             order.addItem(item);
         }
-
         System.out.println();
         System.out.println(order);
-
         sc.close();
     }
 }
