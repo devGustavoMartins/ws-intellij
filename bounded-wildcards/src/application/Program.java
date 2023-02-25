47c3c0546cbb2a9498cf24120193c6c8417ec96f
Gustavo <gustavo.om1206@gmail.com>
2023-02-25 15:05:09 -0300

    commit

---
+++ bounded-wildcards/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(new Rectangle(3.0, 2.0));
+        shapes.add(new Circle(2.0));
+
+        List<Circle> circles = new ArrayList<>();
+        circles.add(new Circle(2.0));
+        circles.add(new Circle(3.0));
+
+        System.out.println("Total area: " + String.format("%.2f", totalArea(shapes)));
+    }
+
+    public static double totalArea(List<? extends Shape> list){
+        double sum = 0.0;
+        for (Shape s : list){
+            sum += s.area();
+        }
+        return sum;
+    }
+}
