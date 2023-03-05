0c43e133e07109c9ad4e6623ec8a19226677e550
Gustavo <gustavo.om1206@gmail.com>
2023-03-05 14:25:30 -0300

    implementação da entidade seller

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Electronics");
-        System.out.println(obj);
+        Seller seller = new Seller(12, "Gustavo", "gustavo@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
