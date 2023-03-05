7b0ec7255e53ea6adb9efdde32d55ecd245c0006
Gustavo <gustavo.om1206@gmail.com>
2023-03-05 14:36:06 -0300

    DaoJDBC e DaoFactory

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,9 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Electronics");
         Seller seller = new Seller(12, "Gustavo", "gustavo@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
