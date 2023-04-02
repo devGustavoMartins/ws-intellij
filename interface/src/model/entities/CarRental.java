61c373f5c1ffc7f9d18b543d5166d94092de29b8
Gustavo <gustavo.om1206@gmail.com>
2023-04-01 23:43:58 -0300

    commit

--- interface/src/model/entities/CarRental.java
+++ interface/src/model/entities/CarRental.java
@@ -8,10 +8,6 @@ public class CarRental {
     private Vehicle vehicle;
     private Invoice invoice;
 
-    public CarRental(){
-
-    }
-
     public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle) {
         this.start = start;
         this.finish = finish;
@@ -22,24 +18,8 @@ public class CarRental {
         return start;
     }
 
-    public void setStart(LocalDateTime start) {
-        this.start = start;
-    }
-
     public LocalDateTime getFinish() {
         return finish;
-    }
-
-    public void setFinish(LocalDateTime finish) {
-        this.finish = finish;
-    }
-
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
-
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
     }
 
     public Invoice getInvoice() {
--- interface/src/model/entities/Invoice.java
+++ interface/src/model/entities/Invoice.java
@@ -5,9 +5,6 @@ public class Invoice {
     private Double basicPayment;
     private Double tax;
 
-    public Invoice() {
-    }
-
     public Invoice(Double basicPayment, Double tax) {
         this.basicPayment = basicPayment;
         this.tax = tax;
@@ -17,16 +14,8 @@ public class Invoice {
         return basicPayment;
     }
 
-    public void setBasicPayment(Double basicPayment) {
-        this.basicPayment = basicPayment;
-    }
-
     public Double getTax() {
         return tax;
-    }
-
-    public void setTax(Double tax) {
-        this.tax = tax;
     }
 
     public Double getTotalPayment() {
