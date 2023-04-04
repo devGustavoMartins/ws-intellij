c29002a7ae44d37f6f06c20cc444954d1f27004d
Gustavo <gustavo.om1206@gmail.com>
2023-04-04 18:16:40 -0300

    UserDTO

--- workshopmongodb/src/main/java/com/devgustavo/workshopmongodb/resources/UserResource.java
+++ workshopmongodb/src/main/java/com/devgustavo/workshopmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.devgustavo.workshopmongodb.resources;
 
 import com.devgustavo.workshopmongodb.domain.User;
+import com.devgustavo.workshopmongodb.dto.UserDTO;
 import com.devgustavo.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,8 +23,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> dtoList = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(dtoList);
     }
 }
