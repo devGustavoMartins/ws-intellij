1335b9b882547efd6957301ca84e78112b6e0230
Gustavo <gustavo.om1206@gmail.com>
2023-08-05 00:29:06 -0300

    commit

--- workshopmongodb/src/main/java/com/devgustavo/workshopmongodb/services/exception/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.devgustavo.workshopmongodb.services.exception;
-
-public class ObjectNotFoundException extends RuntimeException{
-    public ObjectNotFoundException(String msg){
-        super(msg);
-    }
-}
